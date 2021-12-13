0511ea7e85fcab10bc713024e247a1d299946eac
Ebisu02 <71516051+Ebisu02@users.noreply.github.com>
2021-12-13 12:58:39 +0700

    -add b2tree -add search by key in b2tree

--- cursWorkSIAOD/main.cpp
+++ cursWorkSIAOD/main.cpp
@@ -10,6 +10,183 @@ const int Size = 4000;
 
 // k = god poseleniya
 
+class tNode
+
+{
+public:
+	int data;
+	tNode* next;
+
+	tNode() :data()
+	{
+		next = NULL;
+	}
+
+	tNode(int value)
+	{
+		data = value;
+		next = NULL;
+	}
+
+	tNode(int value, tNode* next_pointer)
+	{
+		data = value;
+		next = next_pointer;
+	}
+};
+
+class tList
+{
+protected:
+	static int count;
+
+public:
+	tNode* head;
+	tList()
+	{
+		head = NULL;
+		count++;
+	}
+
+	virtual void push() = 0;
+	virtual void push(int value) = 0;
+	virtual void push(int value, tNode* next_pointer) = 0;
+	virtual void clear() = 0;
+
+	void deleteHead()
+	{
+		try
+		{
+			if (head == NULL) throw "Nothing to delete";
+
+			tNode* p = head->next;
+			delete head;
+			head = p;
+		}
+		catch (const char* exception)
+		{
+			cerr << "Error: " << exception << '\n';
+		}
+	};
+	void print()
+	{
+		try
+		{
+			if (head == NULL) throw "Nothing to print";
+			tNode* pos = head;
+
+			while (pos != NULL)
+			{
+				cout << pos->data << " ";
+				pos = pos->next;
+			}
+			cout << "\n";
+		}
+		catch (const char* exception)
+		{
+			cerr << "Error: " << exception << '\n';
+		}
+	};
+	int getCount()
+	{
+		return count;
+	};
+};
+
+int tList::count = 0;
+
+class tQueue : public tList
+{
+protected:
+	tNode* tail;
+
+public:
+	tQueue()
+	{
+		tail = (tNode*)&head;
+	}
+
+	~tQueue()
+	{
+		if (head != NULL) clear();
+		count--;
+	}
+
+	virtual void push()
+	{
+		tNode* p = new tNode();
+
+		if (head != NULL)
+		{
+			tail->next = p;
+		}
+		else
+		{
+			head = p;
+		}
+
+		tail = p;
+	};
+	virtual void push(int value)
+	{
+		tNode* p = new tNode(value);
+
+		if (head != NULL)
+		{
+			tail->next = p;
+		}
+		else
+		{
+			head = p;
+		}
+
+		tail = p;
+	};
+	virtual void push(int value, tNode* next_pointer)
+	{
+		tNode* p = new tNode(value);
+
+		if (head != NULL)
+		{
+			tail->next = p;
+		}
+		else
+		{
+			head = p;
+		}
+
+		tail = p;
+
+		p = next_pointer;
+		tail->next = p;
+		tail = p;
+	};
+	virtual void clear()
+	{
+		try
+		{
+			if (head == NULL) throw "Nothing to free";
+
+			tNode* pos, * q;
+			q = pos = head;
+
+			while (pos != NULL)
+			{
+				pos = q->next;
+				delete q;
+				q = pos;
+			}
+
+			head = NULL;
+			tail = NULL;
+		}
+		catch (const char* exception)
+		{
+			cerr << "Error: " << exception << '\n';
+		}
+	};
+};
+
 // bd - nas punkt
 struct record
 {
@@ -18,12 +195,6 @@ struct record
 	short int numOfHouse; // nomer doma
 	short int numOfFloor; // nomer kvartiri
 	char dateOfSettling[10]; // data zaseleniya // Primer - 29-02-65 // Format: dd-mm-yy
-};
-
-// Ñëîâàðü...
-enum Order {
-	BYDATEOFSETTLING,
-	BYSTREETNAME
 };
 
 // ×åñòíî - õåð çíàåò çà÷åì ÿ ýòî âûñðàë, ïûòàëñÿ êàê-òî ñðàâíèòü äàòó â ôîðìàòå dd-mm-yy ïîëíîñòüþ
@@ -35,114 +206,114 @@ int charToInt(char* simbol1)
 
 // Òîæå ñîðòèðîâêà, à èìåííî ôîðìèðîâàíèå êó÷è... Áûëî ëåíü äåëàòü 2 îòäåëüíûå äëÿ 2 ðàçíûõ ñëó÷àåâ, ïîýòîìó ñäåëàë ñëîâàðü
 // Äëÿ äàííîé ñèòó¸âèíû, î÷åíü ëåíèâûé ÿ, óæ èçâèíèòå, çíàþ, ÷òî ýòî ïëîõîé òîí =)
-void makeHeap(int arr[], int size, int i, record* data, Order order)
+void makeHeap(int arr[], int size, int i, record* data)
 {
 	int largest = i;
 	int l = 2 * i + 1;
 	int r = 2 * i + 2;
 
 
-	if (order == BYDATEOFSETTLING)
+	char bufForLargestY[2];
+	char bufForLargestM[2];
+	char bufForLargestD[2];
+
+	bufForLargestY[0] = data[arr[largest]].dateOfSettling[6];
+	bufForLargestY[1] = data[arr[largest]].dateOfSettling[7];
+	bufForLargestM[0] = data[arr[largest]].dateOfSettling[3];
+	bufForLargestM[1] = data[arr[largest]].dateOfSettling[4];
+	bufForLargestD[0] = data[arr[largest]].dateOfSettling[0];
+	bufForLargestD[1] = data[arr[largest]].dateOfSettling[1];
+
+	long int cLargest = charToInt(bufForLargestY) * 12 * 30 + charToInt(bufForLargestM) * 30 + charToInt(bufForLargestD);
+
+
+	if (l < size)
 	{
-		char bufForLargestY[2];
-		char bufForLargestM[2];
-		char bufForLargestD[2];
+		char bufForLY[2];
+		char bufForLM[2];
+		char bufForLD[2];
 
-		bufForLargestY[0] = data[arr[largest]].dateOfSettling[6];
-		bufForLargestY[1] = data[arr[largest]].dateOfSettling[7];
-		bufForLargestM[0] = data[arr[largest]].dateOfSettling[3];
-		bufForLargestM[1] = data[arr[largest]].dateOfSettling[4];
-		bufForLargestD[0] = data[arr[largest]].dateOfSettling[0];
-		bufForLargestD[1] = data[arr[largest]].dateOfSettling[1];
+		bufForLY[0] = data[arr[l]].dateOfSettling[6];
+		bufForLY[1] = data[arr[l]].dateOfSettling[7];
+		bufForLM[0] = data[arr[l]].dateOfSettling[3];
+		bufForLM[1] = data[arr[l]].dateOfSettling[4];
+		bufForLD[0] = data[arr[l]].dateOfSettling[0];
+		bufForLD[1] = data[arr[l]].dateOfSettling[1];
 
-		long int cLargest = charToInt(bufForLargestY) * 12 * 30 + charToInt(bufForLargestM) * 30 + charToInt(bufForLargestD);
+		long int cL = charToInt(bufForLY) * 12 * 30 + charToInt(bufForLM) * 30 + charToInt(bufForLD);
 
-
-		if (l < size)
+		if (cL == cLargest)
 		{
-			char bufForLY[2];
-			char bufForLM[2];
-			char bufForLD[2];
-
-			bufForLY[0] = data[arr[l]].dateOfSettling[6];
-			bufForLY[1] = data[arr[l]].dateOfSettling[7];
-			bufForLM[0] = data[arr[l]].dateOfSettling[3];
-			bufForLM[1] = data[arr[l]].dateOfSettling[4];
-			bufForLD[0] = data[arr[l]].dateOfSettling[0];
-			bufForLD[1] = data[arr[l]].dateOfSettling[1];
-
-			long int cL = charToInt(bufForLY) * 12 * 30 + charToInt(bufForLM) * 30 + charToInt(bufForLD);
-
-			if (cL > cLargest)
+			if (l < size && data[arr[l]].streetAdress[0] >= data[arr[largest]].streetAdress[0])
 			{
 				largest = l;
 			}
 		}
-
-		bufForLargestY[0] = data[arr[largest]].dateOfSettling[6];
-		bufForLargestY[1] = data[arr[largest]].dateOfSettling[7];
-		bufForLargestM[0] = data[arr[largest]].dateOfSettling[3];
-		bufForLargestM[1] = data[arr[largest]].dateOfSettling[4];
-		bufForLargestD[0] = data[arr[largest]].dateOfSettling[0];
-		bufForLargestD[1] = data[arr[largest]].dateOfSettling[1];
-
-		cLargest = (charToInt(bufForLargestY) * 12 * 30) + (charToInt(bufForLargestM) * 30) + charToInt(bufForLargestD);
-
-		if (r < size)
+		if (cL > cLargest)
 		{
+			largest = l;
+		}
+	}
 
-			char bufForRY[2];
-			char bufForRM[2];
-			char bufForRD[2];
+	bufForLargestY[0] = data[arr[largest]].dateOfSettling[6];
+	bufForLargestY[1] = data[arr[largest]].dateOfSettling[7];
+	bufForLargestM[0] = data[arr[largest]].dateOfSettling[3];
+	bufForLargestM[1] = data[arr[largest]].dateOfSettling[4];
+	bufForLargestD[0] = data[arr[largest]].dateOfSettling[0];
+	bufForLargestD[1] = data[arr[largest]].dateOfSettling[1];
 
-			bufForRY[0] = data[arr[r]].dateOfSettling[6];
-			bufForRY[1] = data[arr[r]].dateOfSettling[7];
-			bufForRM[0] = data[arr[r]].dateOfSettling[3];
-			bufForRM[1] = data[arr[r]].dateOfSettling[4];
-			bufForRD[0] = data[arr[r]].dateOfSettling[0];
-			bufForRD[1] = data[arr[r]].dateOfSettling[1];
+	cLargest = (charToInt(bufForLargestY) * 12 * 30) + (charToInt(bufForLargestM) * 30) + charToInt(bufForLargestD);
 
-			long int cR = charToInt(bufForRY) * 12 * 30 + charToInt(bufForRM) * 30 + charToInt(bufForRD);
+	if (r < size)
+	{
 
-			if (cR > cLargest)
+		char bufForRY[2];
+		char bufForRM[2];
+		char bufForRD[2];
+
+		bufForRY[0] = data[arr[r]].dateOfSettling[6];
+		bufForRY[1] = data[arr[r]].dateOfSettling[7];
+		bufForRM[0] = data[arr[r]].dateOfSettling[3];
+		bufForRM[1] = data[arr[r]].dateOfSettling[4];
+		bufForRD[0] = data[arr[r]].dateOfSettling[0];
+		bufForRD[1] = data[arr[r]].dateOfSettling[1];
+
+		long int cR = charToInt(bufForRY) * 12 * 30 + charToInt(bufForRM) * 30 + charToInt(bufForRD);
+		if (cR == cLargest)
+		{
+			if (r < size && data[arr[r]].streetAdress[0] >= data[arr[largest]].streetAdress[0])
 			{
 				largest = r;
 			}
 		}
-	}
-
-
-	if (order == BYSTREETNAME) {
-		if (l < size && data[arr[l]].streetAdress[0] >= data[arr[largest]].streetAdress[0])
-		{
-			largest = l;
-		}
-		if (r < size && data[arr[r]].streetAdress[0] >= data[arr[largest]].streetAdress[0])
+		if (cR > cLargest)
 		{
 			largest = r;
 		}
+
 	}
+
 
 
 	if (largest != i)
 	{
 		swap(arr[i], arr[largest]);
-		makeHeap(arr, size, largest, data, order);
+		makeHeap(arr, size, largest, data);
 	}
 
 }
 
 // Ñîðòèðîâêà, ïèðàìèäàëüíàÿ, íå ïîäõîäèò îíà òóò âîîáùå... Íî... Òàêîå çàäàíèå =)
-void indexSort(record* data, int* arr, int size, Order order)
+void indexSort(record* data, int* arr, int size)
 {
 	for (int i = size / 2 - 1; i >= 0; --i)
 	{
-		makeHeap(arr, size, i, data, order);
+		makeHeap(arr, size, i, data);
 	}
 	for (int i = size - 1; i >= 0; --i)
 	{
 		swap(arr[0], arr[i]);
-		makeHeap(arr, i, 0, data, order);
+		makeHeap(arr, i, 0, data);
 	}
 }
 
@@ -179,13 +350,121 @@ void printRecordByIndexArray(int* indexArray, record* recordBuffer, int size)
 	}
 }
 
+struct Vertex {
+	int data;
+	int balance;
+	Vertex* left, * right;
+};
 
-// Ïîèñê, âûâîäèò òàáëèöó ýëåìåíòîâ, íàéäåííûõ ïî çàäàííîìó êëþ÷ó
+int VR, HR;
+
+void make_b2tree(int D, Vertex*& p)
+{
+	if (!p)
+	{
+		p = new Vertex;
+		p->data = D;
+		p->left = p->right = NULL;
+		p->balance = 0;
+		VR = 1;
+	}
+	else if (p->data > D)
+	{
+		make_b2tree(D, p->left);
+		if (VR == 1)
+		{
+			if (p->balance == 0)
+			{
+				Vertex* q = p->left;
+				p->left = q->right;
+				q->right = p;
+				p = q;
+				p->balance = 1;
+				VR = 0;
+				HR = 1;
+			}
+			else
+			{
+				p->balance = 0;
+				VR = 1;
+				HR = 0;
+			}
+		}
+		else
+			HR = 0;
+	}
+	else if (p->data < D)
+	{
+		make_b2tree(D, p->right);
+		if (VR == 1)
+		{
+			p->balance = 1;
+			HR = 1;
+			VR = 0;
+		}
+		else if (HR == 1)
+		{
+			if (p->balance == 1)
+			{
+				Vertex* q = p->right;
+				p->right = q->left;
+				p->balance = 0;
+				q->balance = 0;
+				q->left = p;
+				p = q;
+				VR = 1;
+				HR = 0;
+			}
+			else
+			{
+				HR = 0;
+			}
+		}
+	}
+}
+
+int counterp = 0;
+
+void printFromLeftToRight(Vertex* p, int* indexArray, record* d) {
+	
+	if (p) {
+		printFromLeftToRight(p->left, indexArray, d);
+		if (counterp < 20)
+		{
+			++counterp;
+			cout << d[indexArray[p->data]].fullName << " \t| " << d[indexArray[p->data]].streetAdress << " \t| " << d[indexArray[p->data]].numOfHouse << " \t| " << d[indexArray[p->data]].numOfFloor << " \t| " << d[indexArray[p->data]].dateOfSettling << "\n";
+		}
+		else
+		{
+			cout << "\nIf u wanna see next page - write '1', if not - write '0'\n";
+			int chs = -1;
+			cin >> chs;
+			if (chs == 0)
+			{
+				return;
+			}
+			else if (chs == 1)
+			{
+				counterp = 0;
+				goto c;
+			}
+			else
+			{
+				cout << "\nInvalid choice\n";
+			}
+		}
+		c:
+		printFromLeftToRight(p->right, indexArray, d);
+	}
+}
+
 void findByYearOfSettling(int* indexArray, record* data, int size, char* key)
 {
 	int left = 0;
 	int right = size;
+	int counter = 0;
 	int amountOfVariants = 0;
+	tQueue* queue = new tQueue();
 	while (left <= right)
 	{
 		int mid = (left + right) / 2;
@@ -209,7 +488,7 @@ void findByYearOfSettling(int* indexArray, record* data, int size, char* key)
 					if (bufForCompareLeft[0] == key[0] && bufForCompareLeft[1] == key[1])
 					{
 						++amountOfVariants;
-						cout << data[indexArray[gLeft]].fullName << " | " << data[indexArray[gLeft]].streetAdress << " | " << data[indexArray[gLeft]].numOfHouse << " \t | " << data[indexArray[gLeft]].numOfFloor << "\t      | " << data[indexArray[gLeft]].dateOfSettling << "      |" << "\n";
+						queue->push(gLeft);
 						--gLeft;
 					}
 					else {
@@ -221,7 +500,8 @@ void findByYearOfSettling(int* indexArray, record* data, int size, char* key)
 					fLeft = false;
 				}
 			}
-			cout << data[indexArray[mid]].fullName << " | " << data[indexArray[mid]].streetAdress << " | " << data[indexArray[mid]].numOfHouse << " \t | " << data[indexArray[mid]].numOfFloor << "\t      | " << data[indexArray[mid]].dateOfSettling << "      |" << "\n";
+			++counter;
+			queue->push(mid);
 			bool fRight = true;
 			int gRight = mid + 1;
 			while (fRight)
@@ -234,7 +514,7 @@ void findByYearOfSettling(int* indexArray, record* data, int size, char* key)
 					if (bufForCompareRight[0] == key[0] && bufForCompareRight[1] == key[1])
 					{
 						++amountOfVariants;
-						cout << data[indexArray[gRight]].fullName << " | " << data[indexArray[gRight]].streetAdress << " | " << data[indexArray[gRight]].numOfHouse << " \t | " << data[indexArray[gRight]].numOfFloor << "\t      | " << data[indexArray[gRight]].dateOfSettling << "      |" << "\n";
+						queue->push(gRight);
 						++gRight;
 					}
 					else
@@ -247,7 +527,19 @@ void findByYearOfSettling(int* indexArray, record* data, int size, char* key)
 					fRight = false;
 				}
 			}
+			Vertex* btree = nullptr;
+			tNode* cur = queue->head;
+			while (cur->next)
+			{
+				make_b2tree(cur->data, btree);
+				cur = cur->next;
+			}
+			printFromLeftToRight(btree, indexArray, data);
 			cout << "\n\nWas found " << amountOfVariants << " of variants...\n";
+			queue->clear();
+			delete cur;
+			delete queue;
+			delete btree;
 			return;
 		}
 		if (charToInt(bufForCompare) < charToInt(key))
@@ -292,8 +584,6 @@ void printRecord(record* Array, int size)
 	}
 }
 
-void make_binary_b_tree()
-
 void menu()
 {
 	int arr[Size];
@@ -308,28 +598,18 @@ void menu()
 		indArray[i] = i;
 	}
 
-	cout << "Menu\n\nWrite a number of point what u want to use\n1.Print records\n2.Print sorted records by:\n\t1)Date of settling\n\t2)Name of street\n3)Find a records by key\n0.Exit\n";
+	cout << "Menu\n\nWrite a number of point what u want to use\n1.Print records\n2.Print sorted records\n3.Find a records by key\n0.Exit\n";
 	int choose = 0;
 	while (cin >> choose)
 	{
-		cout << "\n\nWrite a number of point what u want to use\n1.Print records\n2.Print sorted records by:\n\t1)Date of settling\n\t2)Name of street\n3)Find a records by key\n0.Exit\n";
+		cout << "\n\nWrite a number of point what u want to use\n1.Print records\n2.Print sorted records\n3.Find a records by key\n0.Exit\n";
 		if (choose == 1)
 		{
 			printRecord(recArray, Size);
 		}
 		if (choose == 2)
 		{
-			cout << "\nChoose how u would like to sort this database:\n1 - By date of settling\n2 - By name of the street\nYour choice = ";
-			int chooseOrder = 0;
-			cin >> chooseOrder; 
-			if (chooseOrder == 1)
-			{
-				indexSort(recArray, indArray, Size, BYDATEOFSETTLING);
-			}
-			if (chooseOrder == 2)
-			{
-				indexSort(recArray, indArray, Size, BYSTREETNAME);
-			}
+			indexSort(recArray, indArray, Size);
 			printRecordByIndexArray(indArray, recArray, Size);
 		}
 		if (choose == 3)
@@ -342,7 +622,7 @@ void menu()
 			{
 				bIndArray[i] = i;
 			}
-			indexSort(recArray, bIndArray, Size, BYDATEOFSETTLING);
+			indexSort(recArray, bIndArray, Size);
 			findByYearOfSettling(bIndArray, recArray, Size, key);
 		}
 		if (choose == 0)
