b5e936d516a2f9538ad843ce10d1920c8b3f8cfd
Ebisu02 <71516051+Ebisu02@users.noreply.github.com>
2021-12-13 17:40:37 +0700

    -add coding Haffman -add b2tree search -finally end my course work

--- cursWorkSIAOD/main.cpp
+++ cursWorkSIAOD/main.cpp
@@ -1,7 +1,12 @@
 ﻿#include <iostream>
 #include <string>
+#include <map>
+#include <cmath>
+#include <list>
 #include <vector>
+#include <fstream>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -196,6 +201,7 @@ struct record
 	short int numOfFloor; // nomer kvartiri
 	char dateOfSettling[10]; // data zaseleniya // Primer - 29-02-65 // Format: dd-mm-yy
 };
+
 
 // ×åñòíî - õåð çíàåò çà÷åì ÿ ýòî âûñðàë, ïûòàëñÿ êàê-òî ñðàâíèòü äàòó â ôîðìàòå dd-mm-yy ïîëíîñòüþ
 // Íî ïîêà ÷òî çàáèë õåð, ò.ê. ñèëüíî âëèÿåò íà òðóäîåìêîñòü è âðåìÿ è ñêîðîñòü è âîîáùå... Åáàë ÿ â ðîò ýòî íà ïëþñàõ äåëàòü!
@@ -425,10 +431,28 @@ void make_b2tree(int D, Vertex*& p)
 
 int counterp = 0;
 
-void printFromLeftToRight(Vertex* p, int* indexArray, record* d) {
-	
+#include <string.h>
+
+void search_in_tree(Vertex* root, char* key, record* data, int* ind) {
+	if (root) {
+		if (key[1] < data[ind[root->data]].dateOfSettling[1]) {
+			search_in_tree(root->left, key, data, ind);
+		}
+		else if (key[1] > data[ind[root->data]].dateOfSettling[7]) {
+			search_in_tree(root->left, key, data, ind);
+		}
+		else if (key[1] == data[ind[root->data]].dateOfSettling[1]) {
+			search_in_tree(root->left, key, data, ind);
+			cout << data[ind[root->data]].fullName << " \t| " << data[ind[root->data]].streetAdress << " \t| " << data[ind[root->data]].numOfHouse << " \t| " << data[ind[root->data]].numOfFloor << " \t| " << data[ind[root->data]].dateOfSettling << "\n";
+			search_in_tree(root->left, key, data, ind);
+		}
+	}
+}
+
+
+void printFromLeftToRight(Vertex* p, int* indexArray, record* d, Vertex* root) {
 	if (p) {
-		printFromLeftToRight(p->left, indexArray, d);
+		printFromLeftToRight(p->left, indexArray, d, root);
 		if (counterp < 20)
 		{
 			++counterp;
@@ -436,6 +460,7 @@ void printFromLeftToRight(Vertex* p, int* indexArray, record* d) {
 		}
 		else
 		{
+			cout << "\nIf u want to find some records by day of settling, then write '2'";
 			cout << "\nIf u wanna see next page - write '1', if not - write '0'\n";
 			int chs = -1;
 			cin >> chs;
@@ -448,13 +473,20 @@ void printFromLeftToRight(Vertex* p, int* indexArray, record* d) {
 				counterp = 0;
 				goto c;
 			}
+			else if (chs == 2)
+			{
+				char ch[8];
+				cout << "\nEnter a date what u wanna find: (format dd-mm-yy)";
+				cin >> ch;
+				search_in_tree(root, ch, d, indexArray);
+			}
 			else
 			{
 				cout << "\nInvalid choice\n";
 			}
 		}
 		c:
-		printFromLeftToRight(p->right, indexArray, d);
+		printFromLeftToRight(p->right, indexArray, d, root);
 	}
 }
 
@@ -534,7 +566,7 @@ void findByYearOfSettling(int* indexArray, record* data, int size, char* key)
 				make_b2tree(cur->data, btree);
 				cur = cur->next;
 			}
-			printFromLeftToRight(btree, indexArray, data);
+			printFromLeftToRight(btree, indexArray, data, btree);
 			cout << "\n\nWas found " << amountOfVariants << " of variants...\n";
 			queue->clear();
 			delete cur;
@@ -584,6 +616,178 @@ void printRecord(record* Array, int size)
 	}
 }
 
+struct symb {
+	char smb;
+	int sum = 0;
+	double pr = 0;
+	int length = 0;
+	char code[32];
+};
+
+double midLength(std::vector<symb>& charNumb) {
+	float sumLen = 0;
+	for (int i = 0; i < charNumb.size(); i++)
+		sumLen += charNumb[i].pr * charNumb[i].length;
+	return sumLen;
+}
+
+double entropy(std::vector<symb>& charNumb) {
+	float entrop = -1;
+	for (int i = 0; i < charNumb.size(); i++)
+		entrop += charNumb[i].pr * log2(charNumb[i].pr);
+	return -entrop;
+}
+
+void sort1(std::vector<symb>& charNumb, int L, int R) {
+	int a = charNumb[L].sum, i = L, j = R;
+	symb t;
+	while (i <= j) {
+		while (charNumb[i].sum > a)i++;
+		while (charNumb[j].sum < a)j--;
+		if (i <= j) {
+			t = charNumb[i];
+			charNumb[i] = charNumb[j];
+			charNumb[j] = t;
+			i++;
+			j--;
+		}
+	}
+	if (L < j) sort1(charNumb, L, j);
+	if (i < R) sort1(charNumb, i, R);
+}
+
+int Up(int n, double q, std::vector<symb>& c, double* chance)
+{
+	int i = 0, j = 0;
+	for (i = n - 2; i >= 1; --i)
+	{
+		if (c[i - 1].pr < q)
+		{
+			c[i].pr = c[i - 1].pr;
+		}
+		else
+		{
+			j = i;
+			break;
+		}
+		if ((i - 1) == 0 && chance[i - 1] < q)
+		{
+			j = 0;
+			break;
+		}
+	}
+	c[j].pr = q;
+	return j;
+}
+
+void Down(int n, int j, std::vector<symb>& c)
+{
+	char p[32];
+	for (int i = 0; i < 32; ++i)
+	{
+		p[i] = c[j].code[i];
+	}
+	int l = c[j].length;
+	for (int i = j; i < n - 2; ++i)
+	{
+		for (int k = 0; k < 32; ++k)
+		{
+			c[i].code[k] = c[i + 1].code[k];
+		}
+		c[i].length = c[i + 1].length;
+	}
+	for (int i = 0; i < 32; ++i)
+	{
+		c[n - 2].code[i] = p[i];
+		c[n - 1].code[i] = p[i];
+	}
+	c[n - 1].code[l] = '1';
+	c[n - 2].code[l] = '0';
+	c[n - 1].length = l + 1;
+	c[n - 2].length = l + 1;
+}
+
+void huffman(int n, double* chance, std::vector<symb>& c)
+{
+	if (n == 2)
+	{
+		c[0].code[0] = '0';
+		c[1].code[0] = '1';
+		c[0].length = 1;
+		c[1].length = 1;
+	}
+	else
+	{
+		double q = chance[n - 2] + chance[n - 1];
+		int j = Up(n, q, c, chance);
+		huffman(n - 1, chance, c);
+		Down(n, j, c);
+	}
+}
+
+void decode(ifstream& f)
+{
+	char ch;
+	int numberOfsymb = 0;
+	std::vector<symb> charNumb(256);
+	f.seekg(0, std::ios::beg);
+
+	while (f.read(&ch, 1)) {
+		charNumb[static_cast<int>(ch) + 128].smb = ch;
+		charNumb[static_cast<int>(ch) + 128].sum++;
+		numberOfsymb++;
+	}
+
+	for (int i = 0; i < charNumb.size(); i++) {
+		if (charNumb[i].sum == 0) {
+			charNumb.erase(charNumb.begin() + i);
+			i--;
+		}
+		else
+			charNumb[i].pr = static_cast<float>(charNumb[i].sum) / numberOfsymb;
+	}
+	std::cout << "Amount: " << charNumb.size() << "\n\n";
+
+	sort1(charNumb, 0, charNumb.size() - 1);
+
+	double* chance_l = new double[numberOfsymb];
+	for (int i = 0; i < charNumb.size(); ++i)
+	{
+		chance_l[i] = charNumb[i].pr;
+	}
+
+
+	huffman(charNumb.size(), chance_l, charNumb);
+
+
+	int count = 0;
+	for (int i = 0; i < charNumb.size(); ++i, ++count) {
+		if (count < 20)
+		{
+			std::cout << "\t" << i + 1 << ") " << "[" << charNumb[i].smb << "]" << "\t" << charNumb[i].sum << "\t\t" << charNumb[i].pr << "\t\t" << charNumb[i].code << "\n";
+		}
+		else
+		{
+			std::cout << "\n Entropy: " << entropy(charNumb) << "\n Medium length: " << midLength(charNumb) << "\n";
+			cout << "\nIf u wanna see next page - write '1', if not - write '0'\n";
+			int chs = -1;
+			cin >> chs;
+			if (chs == 0)
+			{
+				return;
+			}
+			else if (chs == 1)
+			{
+				count = 0;
+			}
+			else
+			{
+				cout << "\nInvalid choice\n";
+			}
+		}
+	}
+}
+
 void menu()
 {
 	int arr[Size];
@@ -598,11 +802,10 @@ void menu()
 		indArray[i] = i;
 	}
 
-	cout << "Menu\n\nWrite a number of point what u want to use\n1.Print records\n2.Print sorted records\n3.Find a records by key\n0.Exit\n";
+	cout << "Menu\n\nWrite a number of point what u want to use\n1.Print records\n2.Print sorted records\n3.Find a records by key\n4.Print code table\n0.Exit\n";
 	int choose = 0;
 	while (cin >> choose)
 	{
-		cout << "\n\nWrite a number of point what u want to use\n1.Print records\n2.Print sorted records\n3.Find a records by key\n0.Exit\n";
 		if (choose == 1)
 		{
 			printRecord(recArray, Size);
@@ -625,10 +828,17 @@ void menu()
 			indexSort(recArray, bIndArray, Size);
 			findByYearOfSettling(bIndArray, recArray, Size, key);
 		}
+		if (choose == 4)
+		{
+			std::ifstream bfile(Path, std::ios::binary);
+			decode(bfile);
+			bfile.close();
+		}
 		if (choose == 0)
 		{
 			exit(1);
 		}
+		cout << "\n\nWrite a number of point what u want to use\n1.Print records\n2.Print sorted records\n3.Find a records by key\n4.Print code table\n0.Exit\n";
 	}
 	fclose(file);
 }
